d677e0d414a98812b96c2ad2a111fed1db2b99ec
Sumeet Malik <64479357+0sumeetmalik@users.noreply.github.com>
2023-02-02 20:03:08 +0530

    Updated till 2nd Feb 2023, 1.36pm
    4 project completed

--- project5.py
+++ project5.py
@@ -1,11 +1,19 @@
+"""
+Created a Node name block which hold various values and created a linkedlist named as Blockchain
+Blockchain linked list has head and tail which track first and last node which contains all block data
+
+"""
+
+
+
 import hashlib
 from datetime import datetime
 
 
 class Block:
 
-    def __init__(self, data, previous_hash):
-        self.timestamp = datetime.now()
+    def __init__(self, data, timestamp, previous_hash):
+        self.timestamp = timestamp
         self.data = data
         if previous_hash is None:
             self.previous_hash = 0
@@ -31,7 +39,8 @@ class LinkedList:
 
     def add_block(self, data, previous_hash=None):
         # Creating block of Data for chain
-        block = Block(data, previous_hash)
+        timestamp = datetime.now()
+        block = Block(data, timestamp, previous_hash)
 
         if self.head is None:
             self.head = block
@@ -57,21 +66,24 @@ if __name__ == '__main__':
     blockchain.add_block('Some Information on Block 0')
     print('Head of blockchain', blockchain.head.data)
     print('Tail of blockchain', blockchain.tail.data)
-    print('Previous hash', blockchain.tail.previous_hash)
-    print('Hash', blockchain.tail.hash)
+    print('Previous hash of Block', blockchain.tail.previous_hash)
+    print('Hash of last block', blockchain.tail.hash)
+    print('Time Stamp of Last Block', blockchain.tail.timestamp)
     print('-' * 50)
     # adding second block fo data
     blockchain.add_block('Some Information on Block 1', blockchain.get_hash_previous_block())
     print('Head of blockchain', blockchain.head.data)
     print('Tail of blockchain', blockchain.tail.data)
-    print('Previous hash', blockchain.tail.previous_hash)
-    print('Hash', blockchain.tail.hash)
+    print('Previous hash of Block', blockchain.tail.previous_hash)
+    print('Hash of last block', blockchain.tail.hash)
+    print('Time Stamp of Last Block', blockchain.tail.timestamp)
     print('-' * 50)
 
     # Adding 3rd block of data
     blockchain.add_block('Some Information on Block 2', blockchain.get_hash_previous_block())
     print('Head of blockchain', blockchain.head.data)
     print('Tail of blockchain', blockchain.tail.data)
-    print('Previous hash', blockchain.tail.previous_hash)
-    print('Hash', blockchain.tail.hash)
+    print('Previous hash of Block', blockchain.tail.previous_hash)
+    print('Hash of last block', blockchain.tail.hash)
+    print('Time Stamp of Last Block', blockchain.tail.timestamp)
     print('-' * 50)
